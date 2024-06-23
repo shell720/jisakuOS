d765414cebfff70e9fa8daa3f84778e2fb1f7a30
増田秀人 <smasuda@masudahidetonoMacBook-Air.local>
2024-06-24 01:11:04 +0900

    マウスカーソルが動かせるように（だたしポーリング）

--- kernel/pci.cpp
+++ kernel/pci.cpp
@@ -16,14 +16,14 @@ namespace{
 
 
     //devices[num_device] に情報を書き込み num_device をインクリメント
-    Error AddDevice(uint8_t bus, uint8_t device, uint8_t function, uint8_t header_type){
+    Error AddDevice(const Device& device){
         if (num_devices == devices.size()){
-            return Error::kFull;
+            return MAKE_ERROR(Error::kFull);
         }
 
-        devices[num_devices] = Device{bus, device, function, header_type};
+        devices[num_devices] = device;
         ++num_devices;
-        return Error::kSuccess;
+        return MAKE_ERROR(Error::kSuccess);
     }
 
     Error ScanBus(uint8_t bus);
@@ -31,23 +31,21 @@ namespace{
     //指定のファンクションをdevicesに追加する
     //もしPCI-PCIブリッジであれば、セカンダリバスに対しScanBusを実行
     Error ScanFunction(uint8_t bus, uint8_t device, uint8_t function){
+        auto class_code = ReadClassCode(bus, device, function);
         auto header_type = ReadHeaderType(bus, device, function);
-        if (auto err = AddDevice(bus, device, function, header_type)){
+        Device dev{bus, device, function, header_type, class_code};
+        if (auto err = AddDevice(dev)){
             return err;
         }
 
-        auto class_code = ReadClassCode(bus, device, function);
-        uint8_t base = (class_code>>24) & 0xffu;
-        uint8_t sub = (class_code>>16) & 0xffu;
-
-        if (base== 0x06u && sub==0x04u){
+        if (class_code.Match(0x06u, 0x04u)){
             //PCI-PCIブリッジの時
             auto bus_numbers = ReadBusNumbers(bus, device, function);
             uint8_t secondary_bus = (bus_numbers >> 8) & 0xffu;
             return ScanBus(secondary_bus);
         }
 
-        return Error::kSuccess;
+        return MAKE_ERROR(Error::kSuccess);
     }
 
     // デバイス番号に対応する各ファンクションをスキャンする
@@ -57,7 +55,7 @@ namespace{
             return err;
         }
         if (IsSingleFunctionDevice(ReadHeaderType(bus, device, 0))){
-            return Error::kSuccess;
+            return MAKE_ERROR(Error::kSuccess);
         }
 
         for (uint8_t function=1; function<8; ++function){
@@ -69,7 +67,7 @@ namespace{
             }
         }
 
-        return Error::kSuccess;
+        return MAKE_ERROR(Error::kSuccess);
     }
 
     // バス番号の各デバイスをスキャンする
@@ -83,7 +81,7 @@ namespace{
                 return err;
             }
         }
-        return Error::kSuccess;
+        return MAKE_ERROR(Error::kSuccess);
     }
 }
 
@@ -115,9 +113,15 @@ namespace pci{
         return (ReadData()>>16)& 0xffu;
     }
 
-    uint32_t ReadClassCode(uint8_t bus, uint8_t device, uint8_t function){
+    ClassCode ReadClassCode(uint8_t bus, uint8_t device, uint8_t function){
         WriteAddress(MakeAddress(bus, device, function, 0x08));
-        return ReadData();
+        auto reg = ReadData();
+        ClassCode cc;
+        cc.base = (reg>>24) & 0xffu;
+        cc.sub = (reg>>16) & 0xffu;
+        cc.interface = (reg>>8) & 0xffu;
+
+        return cc;
     }
 
     uint32_t ReadBusNumbers(uint8_t bus, uint8_t device, uint8_t function){
@@ -137,7 +141,7 @@ namespace pci{
             return ScanBus(0);
         }
 
-        for (uint8_t function=1; function<8; ++function){
+        for (uint8_t function=0; function<8; ++function){
             if (ReadDeviceId(0, 0, function) == 0xffffu){
                 continue;
             }
@@ -146,6 +150,36 @@ namespace pci{
                 return err;
             }
         }
-        return Error::kSuccess;
+        return MAKE_ERROR(Error::kSuccess);
+    }
+
+    uint32_t ReadConfReg(const Device& dev, uint8_t reg_addr){
+        WriteAddress(MakeAddress(dev.bus, dev.device, dev.function, reg_addr));
+        return ReadData();
+    }
+
+    void WriteConfReg(const Device& dev, uint8_t reg_addr, uint32_t value){
+        WriteAddress(MakeAddress(dev.bus, dev.device, dev.function, reg_addr));
+        WriteData(value);
+    }
+
+    WithError<uint64_t> ReadBar(Device& device, unsigned int bar_index){
+        if (bar_index >= 6){
+            return {0, MAKE_ERROR(Error::kIndexOutOfRange)};
+        }
+
+        const auto addr = CalcBarAddress(bar_index);
+        const auto bar = ReadConfReg(device, addr);
+
+        if ((bar & 4u) == 0){
+            return {bar, MAKE_ERROR(Error::kSuccess)};
+        }
+
+        if ((bar_index) >= 5){
+            return {bar, MAKE_ERROR(Error::kIndexOutOfRange)};
+        }
+
+        const auto bar_upper = ReadConfReg(device, addr+4);
+        return {bar | (static_cast<uint64_t>(bar_upper)<<32), MAKE_ERROR(Error::kSuccess)};
     }
 }
--- kernel/pci.hpp
+++ kernel/pci.hpp
@@ -12,6 +12,20 @@ namespace pci {
     const uint16_t kConfigAddress = 0x0cf8; //CONFIG_ADDRESSレジスタのIOポートアドレス
     const uint16_t kConfigData=0x0cfc; //CONFIG_DATAレジスタのIOポートアドレス
 
+    struct ClassCode{
+        uint8_t base, sub, interface;
+
+        bool Match(uint8_t b) {return b==base;}
+        bool Match(uint8_t b, uint8_t s) {return Match(b)&& s==sub;}
+        bool Match(uint8_t b, uint8_t s, uint8_t i) {return Match(b, s)&& i==interface;}
+    };
+
+    // PCIデバイスを操作するための基礎データを格納
+    struct Device {
+        uint8_t bus, device, function, header_type;
+        ClassCode class_code;
+    };
+
     //データの読み書き
     void WriteAddress(uint32_t address); //CONFIG_ADDRESSに整数を書き込む
     void WriteData(uint32_t value); //CONFIG_DATAに整数を書き込む
@@ -20,20 +34,30 @@ namespace pci {
     uint16_t ReadVendorId(uint8_t bus, uint8_t device, uint8_t function);
     uint16_t ReadDeviceId(uint8_t bus, uint8_t device, uint8_t function);
     uint8_t ReadHeaderType(uint8_t bus, uint8_t device, uint8_t function);
-    uint32_t ReadClassCode(uint8_t bus, uint8_t device, uint8_t function); //31-24: ベースクラス2, 23-16:サブクラス
+    ClassCode ReadClassCode(uint8_t bus, uint8_t device, uint8_t function); //31-24: ベースクラス2, 23-16:サブクラス
     uint32_t ReadBusNumbers(uint8_t bus, uint8_t device, uint8_t function); //23-16:サブオーディネイトパス番号
     
+    inline uint16_t ReadVendorId(const Device& dev){
+        return ReadVendorId(dev.bus, dev.device, dev.function);
+    }
+
+    uint32_t ReadConfReg(const Device& dev, uint8_t reg_addr);
+
+    void WriteConfReg(const Device& dev, uint8_t reg_addr, uint32_t value);
+
     bool IsSingleFunctionDevice(uint8_t header_type);
 
-    // PCIデバイスを操作するための基礎データを格納
-    struct Device {
-        uint8_t bus, device, function, header_type;
-    };
-
+    
     inline std::array<Device, 32> devices; //ScanAllBus()により発見されたPCIデバイスの一覧
     inline int num_devices; //deviceの有効な要素の数
 
     //PCIデバイスをバス0から再帰的に全て探索して、devicesの先頭から書き込む
     // 発見したデバイスの数を num_deviceに設定する
     Error ScanAllBus(); 
+
+    constexpr uint8_t CalcBarAddress(unsigned int bar_index){
+        return 0x10 + 4*bar_index;
+    }
+
+    WithError<uint64_t> ReadBar(Device& device, unsigned int bar_index);
 }
