0aa5f2af6914368e520f5f57dc6d1fb5af0fcf76
増田秀人 <smasuda@masudahidetonoMacBook-Air.local>
2024-06-27 22:44:30 +0900

    タイマ割り込みされるように

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -4,6 +4,8 @@
 
 #include <numeric>
 #include <vector>
+#include <deque>
+#include <limits>
 
 #include "frame_buffer_config.hpp"
 #include "memory_map.hpp"
@@ -13,27 +15,16 @@
 #include "console.hpp"
 #include "pci.hpp"
 #include "logger.hpp"
-#include "usb/memory.hpp"
-#include "usb/device.hpp"
-#include "usb/classdriver/mouse.hpp"
 #include "usb/xhci/xhci.hpp"
-#include "usb/xhci/trb.hpp"
 #include "interrupt.hpp"
 #include "asmfunc.h"
-#include "queue.hpp"
 #include "segment.hpp"
 #include "paging.hpp"
 #include "memory_manager.hpp"
 #include "window.hpp"
 #include "layer.hpp"
+#include "message.hpp"
 #include "timer.hpp"
-
-char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
-PixelWriter* pixel_writer;
-
-//コンソールのバッファ
-char console_buf[sizeof(Console)];
-Console* console;
 
 // メッセージ出力する関数
 int printk(const char* format, ...){
@@ -49,250 +40,48 @@ int printk(const char* format, ...){
     return result;
 }
 
-char memory_manager_buf[sizeof(BitmapMemoryManager)];
-BitmapMemoryManager* memory_manager;
+std::shared_ptr<Window> main_window;
+unsigned int main_window_layer_id;
+void InitializeMainWindow(){
+    main_window = std::make_shared<Window>(160, 52, screen_config.pixel_format);
+    DrawWindow(*main_window->Writer(), "Hello Window");
 
-// マウスを描写するレイヤーのIDを保持
-unsigned int mouse_layer_id;
-Vector2D<int> screen_size;
-Vector2D<int> mouse_position;
+    main_window_layer_id = layer_manager->NewLayer().SetWindow(main_window).SetDraggable(true).Move({300, 100}).ID();
 
-void MouseObserver(uint8_t buttons, int8_t displacement_x, int8_t displacement_y){
-    static unsigned int mouse_drag_layer_id = 0;
-    static uint8_t previous_buttons = 0;
-    const auto oldpos = mouse_position;
-
-    // マウスの移動範囲が画面内となるように
-    auto newpos = mouse_position + Vector2D<int>{displacement_x, displacement_y};
-    newpos = ElementMin(newpos, screen_size + Vector2D<int>{-1, -1});
-    mouse_position = ElementMax(newpos, {0, 0});
-
-    const auto posdiff = mouse_position - oldpos;
-
-    layer_manager->Move(mouse_layer_id, mouse_position);
-
-    const bool previous_left_pressed = (previous_buttons & 0x01);
-    const bool left_pressed = (buttons & 0x01);
-    if (!previous_left_pressed && left_pressed){ // 左ボタンを押した瞬間
-        auto layer = layer_manager->FindLayerByPosition(mouse_position, mouse_layer_id);
-        if(layer && layer->IsDraggable()){
-            mouse_drag_layer_id = layer->ID();
-        }
-    } else if (previous_left_pressed && left_pressed){ //左ボタンを押している間
-        if (mouse_drag_layer_id > 0){
-            layer_manager->MoveRelative(mouse_drag_layer_id, posdiff);
-        }
-    } else if (previous_left_pressed && !left_pressed){
-        mouse_drag_layer_id = 0;
-    }
-
-    previous_buttons = buttons;
+    layer_manager->UpDown(main_window_layer_id, std::numeric_limits<int>::max());
 }
 
-void SwitchEhci2Xhci(const pci::Device& xhc_dev){
-    bool intel_ehc_exist = false;
-    for (int i=0; i<pci::num_devices; ++i){
-        if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x20u) && 
-            0x8086 == pci::ReadVendorId(pci::devices[i])){
-                intel_ehc_exist = true;
-                break;
-            }
-    }
-    if (!intel_ehc_exist){
-        return;
-    }
-
-    uint32_t superspeed_ports = pci::ReadConfReg(xhc_dev, 0xdc); //USB3PRM
-    pci::WriteConfReg(xhc_dev, 0xd8, superspeed_ports); // USB3_PSSEN
-    uint32_t ehci2xhci_ports = pci::ReadConfReg(xhc_dev, 0xd4); //XUSB2PRM
-    pci::WriteConfReg(xhc_dev, 0xd0, ehci2xhci_ports); // XUSB2PR
-    Log(kDebug, "SwitchEhci2Xhci: SS = %02, xHCI = %02x\n", superspeed_ports, ehci2xhci_ports);
-}
-
-// xHCI用の割り込みハンドラの定義
-usb::xhci::Controller* xhc;
-struct Message {
-    enum Type {
-        kInterruptXHCI,
-    } type;
-};
-ArrayQueue<Message>* main_queue;
-__attribute__((interrupt))
-void IntHandlerXHCI(InterruptFrame* frame){
-    main_queue->Push(Message{Message::kInterruptXHCI});
-    NotifyEndOfInterrupt();
-}
+std::deque<Message>* main_queue;
 
 // カーネルでのスタック領域の設定
 alignas(16) uint8_t kernel_main_stack[1024*1024];
 
 extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_ref, const MemoryMap& memory_map_ref){
     // スタック領域を、UEFI管理からカーネル管理に移行
-    FrameBufferConfig frame_buffer_config{frame_buffer_config_ref};
     MemoryMap memory_map{memory_map_ref};
 
-    switch (frame_buffer_config.pixel_format){
-        case kPixelRGBResv8BitPerColor:
-            pixel_writer = new(pixel_writer_buf)RGBResv8BitPerColorPixelWriter{frame_buffer_config};
-            break;
-        case kPixelBGRResv8BitPerColor:
-            pixel_writer = new(pixel_writer_buf)BGRResv8BitPerColorPixelWriter{frame_buffer_config};
-            break;
-    }
+    InitializeGraphics(frame_buffer_config_ref);
+    InitializeConsole();
 
-    DrawDesktop(*pixel_writer);
-
-    console = new(console_buf) Console{kDesktopFGColor, kDesktopBGColor};
-    console->SetWriter(pixel_writer);
     printk("Welcome to PEGI OS!\n");
     SetLogLevel(kWarn);
 
+    InitializeSegmentation(); // セグメンテーション用のデータをカーネルで管理
+    InitializePaging(); // ページングテーブルをカーネルで管理
+    InitializeMemoryManager(memory_map); //ヒープ領域の初期化
+    ::main_queue = new std::deque<Message>(32);
+    InitializeInterrupt(main_queue);
+
+    InitializePCI();
+    usb::xhci::Initialize();
+
+    InitializeLayer();
+    InitializeMainWindow();
+    InitializeMouse();
+    layer_manager->Draw({{0,0}, ScreenSize()});
+
     InitializeLAPICTimer();
 
-    // セグメンテーション用のデータをカーネルで管理
-    SetupSegments();
-    const uint16_t kernel_cs = 1<<3;
-    const uint16_t kernel_ss = 2<<3;
-    SetDSAll(0);
-    SetCSSS(kernel_cs, kernel_ss);
-    // ページングテーブルをカーネルで管理
-    SetupIdentityPageTable();
-
-    // 
-    ::memory_manager = new(memory_manager_buf) BitmapMemoryManager;
-
-    const auto memory_map_base = reinterpret_cast<uintptr_t>(memory_map.buffer);
-    uintptr_t available_end = 0;
-    for (uintptr_t iter = memory_map_base; iter < memory_map_base+memory_map.map_size; iter += memory_map.descriptor_size){
-        auto desc = reinterpret_cast<const MemoryDescriptor*>(iter);
-        if (available_end < desc->phycical_start){
-            memory_manager->MarkAllocated(FrameID{available_end/kBytesPerFrame}, (desc->phycical_start-available_end)/kBytesPerFrame);
-        }
-
-        const auto physical_end = desc->phycical_start + desc->number_of_pages*kUEFIPageSize;
-        if (IsAvailable(static_cast<MemoryType>(desc->type))){
-            available_end = physical_end;
-        } else {
-            memory_manager->MarkAllocated(FrameID{desc->phycical_start/kBytesPerFrame}, desc->number_of_pages*kUEFIPageSize/kBytesPerFrame);
-        }
-    }
-    //ヒープ領域の初期化
-    memory_manager->SetMemoryRange(FrameID{1}, FrameID{available_end/kBytesPerFrame});
-    if (auto err = InitializeHeap(*memory_manager)){
-        Log(kError, "failed to allocate pages: %s at %s:%d\n", err.Name(), err.File(), err.Line());
-        exit(1);
-    }
-
-    std::array<Message, 32> main_queue_data;
-    ArrayQueue<Message> main_queue(main_queue_data);
-    ::main_queue = &main_queue;
-
-    //PCIデバイスの一覧を表示
-    auto err = pci::ScanAllBus();
-    Log(kDebug, "ScanAllBus: %s\n", err.Name());
-    for (int i=0; i<pci::num_devices; ++i){
-        const auto& dev = pci::devices[i];
-        auto vendor_id = pci::ReadVendorId(dev);
-        auto class_code = pci::ReadClassCode(dev.bus, dev.device, dev.function);
-        Log(kDebug, "%d.%d.%d vend %04x, class %08x, head %02x\n", dev.bus, dev.device, dev.function, vendor_id, class_code, dev.header_type);
-    }
-
-    //intel製を優先してxHCを探す
-    pci::Device* xhc_dev = nullptr;
-    for (int i=0; i<pci::num_devices; ++i){
-        if (pci::devices[i].class_code.Match(0x0cu, 0x03u, 0x30u)){
-            xhc_dev = &pci::devices[i];
-
-            if (0x8086 == pci::ReadVendorId(*xhc_dev)){
-                break;
-            }
-        }
-    }
-    if (xhc_dev){
-        Log(kInfo, "xHC has been found: %d.%d.%d\n", xhc_dev->bus, xhc_dev->device, xhc_dev->function);
-    }
-
-    // 割り込みベクタ0x40を設定して、IDTをCPUに登録する
-    SetIDTEntry(idt[InterruptVector::kXHCI], MakeIDTAttr(DescriptorType::kInterruptGate, 0), reinterpret_cast<uint64_t>(IntHandlerXHCI), kernel_cs);
-    LoadIDT(sizeof(idt)-1, reinterpret_cast<uintptr_t>(&idt[0]));
-
-    // xHCに対して、MSI割り込みを設定する
-    const uint8_t bsp_local_apic_id = *reinterpret_cast<const uint32_t*>(0xfee00020)  >>24;
-    pci::ConfigureMSIFixedDestination(*xhc_dev, bsp_local_apic_id, pci::MSITriggerMode::kLevel,
-        pci::MSIDeliveryMode::kFixed, InterruptVector::kXHCI, 0);
-
-    //BAR0レジスタの読み込み
-    const WithError<uint64_t> xhc_bar = pci::ReadBar(*xhc_dev, 0);
-    Log(kDebug, "ReadBar: %s\n", xhc_bar.error.Name());
-    const uint64_t xhc_mmio_base = xhc_bar.value & ~static_cast<uint64_t>(0xf);
-    Log(kDebug, "xHC mmio_base = %08lx\n", xhc_mmio_base);
-
-    //xhcの初期化
-    usb::xhci::Controller xhc{xhc_mmio_base};
-    if (0x8086 == pci::ReadVendorId(*xhc_dev)){
-        SwitchEhci2Xhci(*xhc_dev);
-    }
-    {
-        auto err = xhc.Initialize();
-        Log(kDebug, "xhc.Initialize: %s\n", err.Name());
-    }
-    Log(kInfo, "xHC staring\n");
-    xhc.Run();
-
-    ::xhc = &xhc;
-    __asm__("sti");
-
-    //USBポートを調べて接続済みポートの設定を行う
-    usb::HIDMouseDriver::default_observer = MouseObserver;
-    for (int i=1; i<=xhc.MaxPorts(); ++i){
-        auto port = xhc.PortAt(i);
-        Log(kDebug, "Port %d: IsConnected=%d\n", i, port.IsConnected());
-
-        if (port.IsConnected()){
-            if (auto err = ConfigurePort(xhc, port)){
-                Log(kError, "failed to configure port: %s at %s:%d\n", err.Name(), err.File(), err.Line());
-                continue;
-            }
-        }
-    }
-
-    // メイン表示範囲
-    screen_size.x = frame_buffer_config.horizontal_resolution;
-    screen_size.y = frame_buffer_config.vertical_resolution;
-    auto bgwindow = std::make_shared<Window>(screen_size.x, screen_size.y, frame_buffer_config.pixel_format);
-    auto bgwriter = bgwindow->Writer();
-    DrawDesktop(*bgwriter);
-    auto mouse_window = std::make_shared<Window>(kMouseCursorWidth, kMouseCursorHeight, frame_buffer_config.pixel_format);
-    mouse_window->SetTransparentColor(kMouseTransparentColor);
-    DrawMouseCursor(mouse_window->Writer(), {0, 0});
-    mouse_position = {200, 200};
-
-    // Widgetの作成
-    auto main_widget = std::make_shared<Window>(160, 52, frame_buffer_config.pixel_format);
-    DrawWindow(*main_widget->Writer(), "Hello Window");
-
-    auto console_window = std::make_shared<Window>(Console::kColumns*8, Console::kRows*16, frame_buffer_config.pixel_format);
-    console->SetWindow(console_window);
-
-    //スクリーンの作成
-    FrameBuffer screen;
-    if (auto err = screen.Initialize(frame_buffer_config)){
-        Log(kError, "failed to initialize frame buffer: %s at %s:%d\n", err.Name(), err.File(), err.Line());
-    }
-    layer_manager = new LayerManager;
-    layer_manager->SetWriter(&screen);
-
-    auto bglayer_id = layer_manager->NewLayer().SetWindow(bgwindow).Move({0,0}).ID();
-    mouse_layer_id = layer_manager->NewLayer().SetWindow(mouse_window).Move(mouse_position).ID();
-    auto main_widget_layer_id = layer_manager->NewLayer().SetWindow(main_widget).SetDraggable(true).Move({300, 100}).ID();
-    console->SetLayerID(layer_manager->NewLayer().SetWindow(console_window).Move({0,0}).ID());
-    layer_manager->UpDown(bglayer_id, 0);
-    layer_manager->UpDown(console->LayerID(), 1);
-    layer_manager->UpDown(main_widget_layer_id, 2);
-    layer_manager->UpDown(mouse_layer_id, 3);
-    layer_manager->Draw({{0,0}, screen_size});
-
-    // widgetに表示するカウンタの作成
     char str[128];
     unsigned int count = 0;
 
@@ -300,30 +89,29 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
     while (true) {
         ++count;
         sprintf(str, "%010u", count);
-        FillRectangle(*main_widget->Writer(), {24,28}, {8*10,16}, {0xc6,0xc6,0xc6});
-        WriteString(*main_widget->Writer(), {24,28}, str, {0,0,0});
-        layer_manager->Draw(main_widget_layer_id);
+        FillRectangle(*main_window->Writer(), {24,28}, {8*10,16}, {0xc6,0xc6,0xc6});
+        WriteString(*main_window->Writer(), {24,28}, str, {0,0,0});
+        layer_manager->Draw(main_window_layer_id);
 
         __asm__("cli");
-        if (main_queue.Count()==0){
+        if (main_queue->size() ==0){
             __asm__("sti");
             continue;
         }
 
-        Message msg = main_queue.Front();
-        main_queue.pop();
+        Message msg = main_queue->front();
+        main_queue->pop_front();
         __asm__("sti");
 
         switch(msg.type){
-          case Message::kInterruptXHCI:
-              while (xhc.PrimaryEventRing()->HasFront()){
-                  if (auto err = ProcessEvent(xhc)){
-                    Log(kError, "Error whiel ProcessEvent: %s at %s: %d\n", err.Name(), err.File(), err.Line());
-                  }
-              }
-              break;
-          default:
-            Log(kError, "Unknown message type: %d\n", msg.type);
+            case Message::kInterruptXHCI:
+                usb::xhci::ProcessEvents();
+                break;
+            case Message::kInterruptLAPICTimer:
+                printk("Timer interrupt\n");
+                break;
+            default:
+                Log(kError, "Unknown message type: %d\n", msg.type);
         }
     }
 }
