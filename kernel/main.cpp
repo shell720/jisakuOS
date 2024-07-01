310ea2878ca89d232a288b4e861d56beb92f1b28
増田秀人 <smasuda@masudahidetonoMacBook-Air.local>
2024-07-01 22:02:28 +0900

    lsコマンドの実装

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -29,6 +29,7 @@
 #include "keyboard.hpp"
 #include "task.hpp"
 #include "terminal.hpp"
+#include "fat.hpp"
 
 // メッセージ出力する関数
 int printk(const char* format, ...){
@@ -118,6 +119,7 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
     InitializeMemoryManager(memory_map); //ヒープ領域の初期化
     InitializeInterrupt();
 
+    fat::Initialize(volume_image);
     InitializePCI();
 
     InitializeLayer();
@@ -140,22 +142,6 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
     usb::xhci::Initialize();
     InitializeKeyboard();
     InitializeMouse();
-
-    uint8_t* p = reinterpret_cast<uint8_t*>(volume_image);
-    printk("Volume Image:\n");
-    for (int i = 0; i<16; ++i){
-        printk("%04x:", i*16);
-        for (int j=0; j<8; ++j){
-            printk(" %02x", *p);
-            ++p;
-        }
-        printk(" ");
-        for (int j=0; j<8; ++j){
-            printk(" %02x", *p);
-            ++p;
-        }
-        printk("\n");
-    }
 
     char str[128];
 
