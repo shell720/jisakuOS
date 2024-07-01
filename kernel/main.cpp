c900262164f8c6bc54d55c488352b10ecd44551b
増田秀人 <smasuda@masudahidetonoMacBook-Air.local>
2024-07-01 19:54:10 +0900

    volumeの読み出し

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -103,7 +103,7 @@ void InputTextWindow(char c){
 alignas(16) uint8_t kernel_main_stack[1024*1024];
 
 extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_ref, 
-    const MemoryMap& memory_map_ref, const acpi::RSDP& acpi_table){
+    const MemoryMap& memory_map_ref, const acpi::RSDP& acpi_table, void* volume_image){
     // スタック領域を、UEFI管理からカーネル管理に移行
     MemoryMap memory_map{memory_map_ref};
 
@@ -140,6 +140,22 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
     usb::xhci::Initialize();
     InitializeKeyboard();
     InitializeMouse();
+
+    uint8_t* p = reinterpret_cast<uint8_t*>(volume_image);
+    printk("Volume Image:\n");
+    for (int i = 0; i<16; ++i){
+        printk("%04x:", i*16);
+        for (int j=0; j<8; ++j){
+            printk(" %02x", *p);
+            ++p;
+        }
+        printk(" ");
+        for (int j=0; j<8; ++j){
+            printk(" %02x", *p);
+            ++p;
+        }
+        printk("\n");
+    }
 
     char str[128];
 
